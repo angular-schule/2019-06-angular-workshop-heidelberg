42a47db40669a3dbc61420115af471ea9c1eaaef
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2019-06-06 15:23:49 +0200

    routing: configure

--- book-rating/src/app/books/books.module.ts
+++ book-rating/src/app/books/books.module.ts
@@ -7,13 +7,15 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { BookComponent } from './book/book.component';
 import { RepeatDirective } from './shared/repeat.directive';
 import { CreateBookComponent } from './create-book/create-book.component';
+import { BookDetailsComponent } from './book-details/book-details.component';
 
 @NgModule({
   declarations: [
     DashboardComponent,
     BookComponent,
     RepeatDirective,
-    CreateBookComponent
+    CreateBookComponent,
+    BookDetailsComponent
   ],
   imports: [
     CommonModule,
--- book-rating/src/app/books/dashboard/dashboard.component.ts
+++ book-rating/src/app/books/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Book } from '../shared/book';
 import { BookRatingService } from '../shared/book-rating.service';
+import { BookStoreService } from '../shared/book-store.service';
 
 @Component({
   selector: 'br-dashboard',
@@ -11,38 +12,19 @@ export class DashboardComponent implements OnInit {
 
   books: Book[] = [];
 
-  constructor(private bs: BookRatingService) { }
+  constructor(private br: BookRatingService, private bs: BookStoreService) { }
 
   ngOnInit() {
-    this.books = [
-      {
-        isbn: '000',
-        title: 'Angular',
-        description: 'Tolles Buch',
-        rating: 5
-      },
-      {
-        isbn: '111',
-        title: 'AngularJS',
-        description: 'Altes Buch',
-        rating: 3
-      },
-      {
-        isbn: '222',
-        title: 'React',
-        description: 'Altes Buch',
-        rating: 1
-      }
-    ];
+    this.bs.getAll().subscribe(books => this.books = books);
   }
 
   doRateDown(book: Book) {
-    const ratedBook = this.bs.rateDown(book);
+    const ratedBook = this.br.rateDown(book);
     this.updateAndSortList(ratedBook);
   }
 
   doRateUp(book: Book) {
-    const ratedBook = this.bs.rateUp(book);
+    const ratedBook = this.br.rateUp(book);
     // const ratedBook = { ...book, rating: book.rating + 1 };
     this.updateAndSortList(ratedBook);
   }
